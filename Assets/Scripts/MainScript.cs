4e93eb6df1a9fefdfd3e014def6d44b4567176d0
BlueHillock <niklas.poljo@gmail.com>
2021-01-14 16:14:44 +0200

    Tein MainGameScenen ja tein kaksi mainscriptiä joista puhuttiin ykän kaa

---
+++ Assets/Scripts/MainScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour
+{
+    //plays the sounds 
+    //makes the beat visuals etc
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SoundScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript : MonoBehaviour
+{
+    // checks which sound should be played
+
+
+    // Start is called before the first frame update
+    public List<GameObject> sounds;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

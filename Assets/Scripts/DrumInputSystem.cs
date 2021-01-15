3c1634652759727fed712956259ebf2eaa8ddda8
BlueHillock <niklas.poljo@gmail.com>
2021-01-15 17:35:47 +0200

    eka versio touch input koodista

--- Assets/Scripts/DrumInputSystem.cs
+++ Assets/Scripts/DrumInputSystem.cs
@@ -10,14 +10,21 @@ public class DrumInputSystem : MonoBehaviour
     public UnityEvent<DrumInput> anythingInput;
 
     public UnityEvent drum1Input;
+    public UnityEvent drum2Input;
+    public UnityEvent drum3Input;
 
     List<DrumInput> drum1Inputs = new List<DrumInput> { DrumInput.D1Single, DrumInput.D1Double, DrumInput.D1Triple };
+    List<DrumInput> drum2Inputs = new List<DrumInput> { DrumInput.D2Single, DrumInput.D2Double, DrumInput.D2Triple };
+    List<DrumInput> drum3Inputs = new List<DrumInput> { DrumInput.D3Single, DrumInput.D3Double, DrumInput.D3Triple };
 
     public void EnterInput(DrumInput d) {
         anythingInput.Invoke(d);
-        print("Check");
         if (drum1Inputs.Contains(d))
             drum1Input.Invoke();
+        else if (drum2Inputs.Contains(d))
+            drum2Input.Invoke();
+        else if (drum3Inputs.Contains(d))
+            drum3Input.Invoke();
             
     }
 
